5c7d7a5b63deabfe67f20f478ac69c82e4bfe85e
Jeff Hertzler <jeffhertzler@gmail.com>
2017-03-11 00:02:03 -0500

    compose exercise 1

--- exercises/compose/compose_exercises_spec.js
+++ exercises/compose/compose_exercises_spec.js
@@ -1,30 +1,30 @@
 var E = require('./compose_exercises');
 var assert = require("chai").assert;
 
-describe.skip("Compose Exercises", function(){
+describe("Compose Exercises", function(){
   var CARS = E.CARS
 
   it('Exercise 1', function(){
     assert.equal(E.isLastInStock(CARS), false);
   });
 
-  it('Exercise 2', function(){
+  it.skip('Exercise 2', function(){
     assert.equal(E.nameOfFirstCar(CARS), "Ferrari FF");
   });
 
-  it('Exercise 3', function(){
+  it.skip('Exercise 3', function(){
     assert.equal(E.averageDollarValue(CARS), 790700);
   });
 
-  it('Exercise 4', function(){
+  it.skip('Exercise 4', function(){
     assert.deepEqual(E.sanitizeNames(CARS), ['ferrari_ff', 'spyker_c12_zagato', 'jaguar_xkr_s', 'audi_r8', 'aston_martin_one_77', 'pagani_huayra']);
   });
 
-  it('Bonus 1', function(){
+  it.skip('Bonus 1', function(){
     assert.equal(E.availablePrices(CARS), '$700,000.00, $1,850,000.00');
   });
 
-  it('Bonus 2', function(){
+  it.skip('Bonus 2', function(){
     assert.equal(E.fastestCar(CARS), 'Aston Martin One-77 is the fastest');
   });
 });
